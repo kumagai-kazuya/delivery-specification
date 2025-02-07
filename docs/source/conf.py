dede38bf4b9f37c0c44061de4edc688c3356a27b
KUMAGAI Kazuya <kumagai.kazuya@jp.panasonic.com>
2025-02-07 15:27:03 +0900

    マークダウンテスト

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Delivery Specification'
+project = 'test_document'
 copyright = '2025, PSNRD'
 author = 'PSNRD'
 release = '0.0.1'
@@ -14,15 +14,47 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser',                  # Markdownサポート
+    'sphinxcontrib.mermaid',        # Mermaid図表描画
+    'sphinx_markdown_tables',       # Markdownテーブルのサポート
+    'sphinx_copybutton',            # コピーボタンの追加
+    'sphinx_diagrams',              # 図表描画
+    # 他の拡張機能があればここに追加
+]
 
-templates_path = ['_templates']
-exclude_patterns = []
+# -- テーマの設定 ---------------------------------------------------------
+html_theme = 'furo'                 # Furoテーマを使用
 
-language = 'ja'
+# -- ソースファイルの拡張子の設定 -----------------------------------------
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Masterドキュメントの設定 --------------------------------------------
+master_doc = 'index'
 
-html_theme = 'alabaster'
+# -- MyST-Parserの設定 ----------------------------------------------------
+myst_enable_extensions = [
+    "dollarmath",        # インラインおよびブロック数式のサポート
+    "amsmath",           # AMS math機能のサポート
+    "colon_fence",       # コロンフェンスのサポート
+    "deflist",           # 定義リストのサポート
+    "html_admonition",   # HTML警告のサポート
+    "html_image",        # HTML画像のサポート
+]
+
+# -- sphinx_copybuttonの設定 ---------------------------------------------
+copybutton_prompt_text = '>>> '    # コードブロックのプロンプトテキスト
+copybutton_prompt_is_regexp = True  # プロンプトテキストを正規表現として扱う
+
+# -- sphinxcontrib.mermaidの設定 -----------------------------------------
+mermaid_output_format = 'svg'        # Mermaid図の出力形式
+
+# -- sphinx_diagramsの設定 ------------------------------------------------
+# sphinx_diagramsは特別な設定は不要ですが、必要に応じて設定を追加します
+
+# -- HTML静的ファイルの設定 -----------------------------------------------
 html_static_path = ['_static']
+
